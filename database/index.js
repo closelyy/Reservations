926d15f1a658743712f5c28bbb50f8ec0ed5539c
aldajani <boss@bosss-air.lan>
2019-08-26 11:42:07 -0700

    Create schema, fake data, seed script, and API route

--- database/index.js
+++ database/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable max-len */
 /* eslint-disable no-console */
 const mongoose = require('mongoose');
 
@@ -9,43 +10,31 @@ db.once('open', () => {
   console.log('Connection established!');
 });
 
-const reservations = [];
-const hours = [];
-const months = [
-  { month: 'January', days: 31 },
-  { month: 'February', days: 28 },
-  { month: 'March', days: 31 },
-  { month: 'April', days: 30 },
-  { month: 'May', days: 31 },
-  { month: 'June', days: 30 },
-  { month: 'July', days: 31 },
-  { month: 'August', days: 31 },
-  { month: 'September', days: 30 },
-  { month: 'October', days: 31 },
-  { month: 'November', days: 30 },
-  { month: 'December', days: 31 },
-];
-// restaurant hours set as 10am to 10pm
-for (let j = 10; j <= 22; j += 1) {
-  hours.push({
-    hour: j,
-    available: true,
-  });
-}
-for (let i = 0; i < months.length; i += 1) {
-  const dates = [];
-  for (let j = 1; j <= months[i].days; j += 1) {
-    dates.push({ day: j, hours });
-  }
-  months[i].dates = dates;
-}
-for (let i = 0; i < months.length; i += 1) {
-  reservations.push(months[i]);
-}
-
-
 const reservationSchema = mongoose.Schema({
-  restaurantId: { type: Number, unique: true },
+  restaurantId: { type: Number },
   currentDate: { type: Date, default: Date.now },
-  reservations,
+  maxPartySize: { type: Number },
+  reservations: [
+    {
+      month: { type: String },
+      days: { type: Number },
+      dates: [
+        {
+          day: { type: Number },
+          hours: [
+            {
+              month: { type: String },
+              day: { type: Number },
+              hour: { type: String },
+              available: { type: Boolean },
+            },
+          ],
+        },
+      ],
+    },
+  ],
 });
+
+const Reservation = mongoose.model('Reservation', reservationSchema);
+
+module.exports.Reservation = Reservation;
--- server/server.js
+++ server/server.js
@@ -1,14 +1,37 @@
+/* eslint-disable array-callback-return */
 /* eslint-disable no-console */
 const express = require('express');
+
+const bodyParser = require('body-parser');
+
+const db = require('../database/index.js');
 
 const app = express();
 const port = process.env.PORT || 3000;
 
-// app.use(express.urlencoded)
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 app.use(express.static('public'));
 
-app.get('/', (req, res) => {
-  res.status(200);
+// Find all available reservations for a given business (currently a single business)
+app.get('/api/reservations/', (req, res) => {
+  db.Reservation.find((err, business) => {
+    const openTimes = [];
+    if (err) {
+      console.log(err);
+    } else {
+      for (let i = 0; i < business[0].reservations.length; i += 1) {
+        for (let j = 0; j < business[0].reservations[i].dates.length; j += 1) {
+          for (let k = 0; k < business[0].reservations[i].dates[j].hours.length; k += 1) {
+            if (business[0].reservations[i].dates[j].hours[k].available) {
+              openTimes.push(business[0].reservations[i].dates[j].hours[k]);
+            }
+          }
+        }
+      }
+    }
+    res.send(openTimes).status(200);
+  });
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}!`));
